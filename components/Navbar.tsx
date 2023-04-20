6e299954605edf08cbea39024b894db17f98f0b7
PeriYumYum <perilin124@gmail.com>
2023-04-21 00:13:49 +0800

    Refactor: Navbar and Logo, Update Logo svg

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,8 @@
 'use client';
 import Link from 'next/link';
-import Image from 'next/image';
 import clsx from 'clsx';
 import { usePathname } from 'next/navigation';
+import { SVGLogo } from './Logo';
 
 const navItems = {
   '/': { name: 'Home' },
@@ -17,27 +17,18 @@ const navItems = {
   },
 };
 
-const scrollToTop = () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-};
-
-const Logo = () => {
-  return (
-    <Link aria-label='Peri Lin' href='/'>
-      <span className='flex my-2 px-3 py-2' onClick={() => scrollToTop()}>
-        <Image src='/logo.svg' alt='logo' width='0' height='0' className='w-[60px] h-auto' />
-      </span>
-    </Link>
-  );
-};
-//navbar
 export const Navbar = () => {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
   let pathname = usePathname() || '/';
   return (
-    <nav className='fixed z-10 w-full pb-2 md:pb-0 md:mr-8 md:w-36 lg:w-48 xl:w-60 md:h-full shadow-lg bg-stone-300 dark:bg-zinc-700'>
+    <nav className='fixed z-10 w-full md:pb-0 md:mr-8 md:w-36 lg:w-48 xl:w-60 md:h-full shadow-lg bg-stone-300 dark:bg-zinc-700'>
       <div className='ml-3 md:ml-5 lg:ml-16 xl:ml-24 mt-5 md:mt-14 xl:mt-20 font-serif'>
-        <div className='top-5 mb-2 md:mx-0 md:mb-8 '>
-          <Logo />
+        <div className='top-5 md:mx-0 md:mb-8 '>
+          <Link aria-label='Peri Lin' href='/' onClick={scrollToTop}>
+            <SVGLogo />
+          </Link>
         </div>
         <div className='flex overflow-x-auto flex-row md:flex-col mb-2 mt-2 md:mt-0'>
           {Object.entries(navItems).map(([path, { name }]) => {
