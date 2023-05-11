0a56744d233f2a51e07dc1eff53745ada39e9bb3
PeriYumYum <perilin124@gmail.com>
2023-05-11 16:58:26 +0800

    Add theme variable

--- components/DarkModeSwitch.tsx
+++ components/DarkModeSwitch.tsx
@@ -14,16 +14,19 @@ export const DarkModeSwitch = () => {
   if (!mounted) {
     return null;
   }
+
   //toggle theme
   const toggleTheme = () => {
     if (theme === 'light') {
       return setTheme('dark');
+    } else {
+      return setTheme('light');
     }
-    return setTheme('light');
   };
 
   return (
     <div className='fixed z-20 flex w-full justify-end items-end py-3 px-4'>
+      The current theme is {theme}
       <input
         type='checkbox'
         aria-label='Toggle theme'
