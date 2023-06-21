666c4ce44c3d34f9a504674a61c63f9074b3c048
PeriYumYum <perilin124@gmail.com>
2023-06-21 22:20:34 +0800

    Add priority add prefetch attributes for better performance

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -14,7 +14,7 @@ export const ProjectCard = ({
   return (
     <div className='card card-compact shadow-2xl'>
       <figure className='relative aspect-video w-full overflow-hidden'>
-        <Image src={previewImage} fill={true} sizes='100' alt='Preview Image' />
+        <Image src={previewImage} fill={true} sizes='100' alt='Preview Image' priority={true} />
       </figure>
       <div className='card-body'>
         <h2 className='card-title text-start p-0'>{projectName}</h2>
@@ -30,11 +30,11 @@ export const ProjectCard = ({
         </div>
         <div className='card-actions justify-end'>
           {liveDemoLink && (
-            <Link href={liveDemoLink} target='_blank' title='Live Demo'>
+            <Link href={liveDemoLink} target='_blank' title='Live Demo' prefetch={false}>
               <FiExternalLink className='text-3xl link-on-hover dark:text-white' />
             </Link>
           )}
-          <Link href={githubLink} target='_blank' title='Source Code on GitHub'>
+          <Link href={githubLink} target='_blank' title='Source Code on GitHub' prefetch={false}>
             <FiGithub className='text-3xl link-on-hover dark:text-white' />
           </Link>
         </div>
