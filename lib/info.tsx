eeb213f570391b7441ee3ca85f901467787b9436
PeriYumYum <perilin124@gmail.com>
2023-06-25 16:38:23 +0800

    Create variables to store string data

--- lib/info.tsx
+++ lib/info.tsx
@@ -1,11 +1,14 @@
+/* homepage */
 export const name = 'Peri Lin';
 export const intro = () => {
   return (
     <>
-      {`Hello, 我是 Peri,  一個自學的前端工程師，我喜歡打造兼具美感和實用性的網站，樂於嘗試網頁相關的新技術 providing sevices for programming needs. Contact me down below and let's get cracking!`}
+      {`Hello, 我是 Peri,  一個自學的前端工程師，我喜歡打造兼具美感和實用性的網站，樂於嘗試網頁相關的新技術 I providing sevices for programming needs. Contact me down below and let's get cracking!`}
     </>
   );
 };
+export const resumeLink = ' ';
+
 export const about = () => {
   return (
     <>
@@ -14,6 +17,23 @@ export const about = () => {
   );
 };
 
+export const playlist = 'https://my-youtube-collection.vercel.app/';
+
+/* projects */
 export const projectsIntro = () => {
   return <>{`Some of the side projects I've worked on:`}</>;
 };
+
+/* contact */
+export const contactIntro = () => {
+  return (
+    <>{`I'm currently open for a fulltime Frontend Developer role. If you want to discuss about
+    that, feel free to contact me.`}</>
+  );
+};
+
+export const email = 'periondev@gmail.com';
+
+export const mailTo = 'mailto:' + email;
+
+export const myGithub = 'https://github.com/periondev';
