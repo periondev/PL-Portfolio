f7bf479f171a0ed3b160f39809ada914bb04764c
PeriYumYum <perilin124@gmail.com>
2023-06-25 16:51:54 +0800

    Update data

--- lib/postData.tsx
+++ lib/postData.tsx
@@ -2,6 +2,14 @@ import { PostType } from './types';
 
 export const posts: PostType[] = [
   {
+    postTitle: 'Hello Post! Lorem ipsum dolor',
+    description:
+      'Consectetur adipiscing elit. Nunc finibus sapien eget quam posuere, vitae consequat tortor eleifend. Proin imperdiet fringilla tempus. Integer eget vulputate ante. Etiam fermentum diam a velit tempor',
+    postLink: 'https://hackmd.io/@pericode/rk-Z-puNn',
+    tools: ['Next.js', 'React', 'Tailwind CSS'],
+    postDate: 'May 10, 2023',
+  },
+  {
     postTitle: 'Title 1',
     description:
       "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
@@ -18,11 +26,12 @@ export const posts: PostType[] = [
     postDate: 'Jan 14, 2023',
   },
   {
-    postTitle: 'Title 3',
+    postTitle:
+      '我是如何在 Next.js 13 with appDir 設定深色模式按鈕 Dark Mode Switch with Tailwind CSS and Daisy UI',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis feugiat metus. Proin nec justo nec augue accumsan suscipit ac feugiat eros. Nulla felis quam, bibendum nec luctus vitae',
+      '隨著深色模式(Dark Mode)在現代網頁中越來越受歡迎，在網頁中提供Dark Mode切換功能好像是一種越來越常見的設計趨勢。而Next.js是非常受歡迎的的React網頁框架，在最新版Next.js 13提供appDir作為beta版的功能，其特色是打造更少客戶端(Client)的JS程式碼，建構...',
     postLink: ' ',
-    tools: ['Next.js', 'React', 'Node.js', 'Tailwind CSS', 'TypeScript'],
-    postDate: 'Nov 02, 2022',
+    tools: ['Next.js', 'React', 'Tailwind CSS', 'daisyUI'],
+    postDate: 'May 12, 2023',
   },
 ];
