81eb65bf5b99f5d62bde6b6150ebcb67029184bb
PeriYumYum <perilin124@gmail.com>
2023-06-29 16:43:53 +0800

    Create truncateText function to limit post display length

--- components/Post.tsx
+++ components/Post.tsx
@@ -2,13 +2,22 @@ import Link from 'next/link';
 import { PostType } from '@lib/types';
 
 export const Post = ({ postTitle, description, postLink, tools, postDate }: PostType) => {
+  const truncateText = (text: string, maxLength: number) => {
+    if (text.length > maxLength) {
+      return text.slice(0, maxLength) + '...';
+    } else {
+      return text;
+    }
+  };
+  const truncatedText = truncateText(description, 140);
+
   return (
-    <div className='card card-compact shadow-lg border-4 border-orange-200 dark:border-zinc-600'>
+    <div className='card card-compact shadow-lg border-4 border-gray-300 dark:border-zinc-600'>
       <div className='card-body'>
         <Link href={postLink} target='_blank' title='See full post'>
           <h2 className='card-title text-start p-0 hover:underline'>{postTitle}</h2>
         </Link>
-        <p>{description}</p>
+        <p>{truncatedText}</p>
         <div className='flex flex-wrap flex-row gap-1.5'>
           {tools.map((tech: string, index: number) => {
             return (
