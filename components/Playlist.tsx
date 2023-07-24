fad396c7f95bd312857a0b4c3e22f840db7ef2fd
PeriYumYum <perilin124@gmail.com>
2023-07-24 15:00:12 +0800

    Update style

--- components/Playlist.tsx
+++ components/Playlist.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link';
 const playlist = 'https://my-youtube-collection.vercel.app/';
 export const Playlist = () => {
   return (
-    <div className='flex justify-center p-6 my-3 rounded-lg border-8'>
+    <div className='card items-center p-5 my-3 border-4 border-zinc-300 dark:border-zinc-600'>
       <Link href={playlist} target='_blank' title='My Youtube Collection'>
-        <p className='link link-hover text-xl'>My Youtube Collection🎬🎵</p>
+        <h3 className='link link-hover text-xl'>My Youtube Collection🎬🎵</h3>
       </Link>
     </div>
   );
