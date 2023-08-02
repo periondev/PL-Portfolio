26986fa894aef64aea468fcac77a45c3408b62cd
PeriYumYum <perilin124@gmail.com>
2023-08-02 23:15:47 +0800

    Update responsive layout of card and tech badge color

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,7 +1,13 @@
 import Link from 'next/link';
 import { PostType } from '@lib/types';
 
-export const Post = ({ postTitle, description, postLink, tags, postDate }: PostType) => {
+export const Post = ({
+  postTitle,
+  description,
+  postLink,
+  tags,
+  postDate,
+}: PostType) => {
   const truncateText = (text: string, maxLength: number) => {
     if (text.length > maxLength) {
       return text.slice(0, maxLength) + '...';
@@ -12,16 +18,18 @@ export const Post = ({ postTitle, description, postLink, tags, postDate }: PostT
   const truncatedText = truncateText(description, 140);
 
   return (
-    <div className='card card-compact shadow-lg border-4 border-gray-300 dark:border-zinc-600'>
+    <div className='card card-compact md:card-normal my-2 shadow-lg border-2 border-gray-300 dark:border-zinc-600'>
       <div className='card-body'>
         <Link href={postLink} target='_blank' title='See full post'>
-          <h2 className='card-title text-start p-0 hover:underline'>{postTitle}</h2>
+          <h2 className='card-title text-start p-0 hover:underline'>
+            {postTitle}
+          </h2>
         </Link>
         <p>{truncatedText}</p>
         <div className='flex flex-wrap flex-row gap-1.5'>
           {tags?.map((tech: string, index: number) => {
             return (
-              <div key={index} className='badge badge-outline'>
+              <div key={index} className='badge badge-info dark:badge-neutral'>
                 {tech}
               </div>
             );
