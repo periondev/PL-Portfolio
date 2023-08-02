3d9a5c35bff2b855f67f219c6bc15708d85e7846
PeriYumYum <perilin124@gmail.com>
2023-08-02 23:29:06 +0800

    Create daisyUI mytheme colors, remove unused fonts and theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,23 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        Zcool: ['ZCOOL XiaoWei', 'serif'],
-      },
-      colors: {
-        'rice-color': '#f6eee3',
-        'lemon-color': '#efdb5e',
-      },
-    },
-  },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   darkMode: 'class',
   daisyui: {
-    themes: ['garden'],
+    themes: [
+      {
+        mytheme: {
+          primary: '#155e75',
+          secondary: '#F000B8',
+          accent: '#37CDBE',
+          neutral: '#374151',
+          'base-100': '#FFFFFF',
+          info: '#e0e7ff',
+          success: '#36D399',
+          warning: '#FBBD23',
+          error: '#F87272',
+        },
+      },
+    ],
   },
 };
