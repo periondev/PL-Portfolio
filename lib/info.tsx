50a26d7407575df04a3ca55a445065918b8f08bf
PeriYumYum <perilin124@gmail.com>
2023-08-06 00:08:02 +0800

    Create About component, update info

--- lib/info.tsx
+++ lib/info.tsx
@@ -1,16 +1,16 @@
+/*Personal Information*/
 /* homepage */
 export const name = 'Peri Lin';
 
-export const intro = `👋Hello，我是Peri，一位自學的前端工程師。打造美感與實用兼備的網站是我的熱情所在！歡迎一起開創網路世界！`;
+export const intro = `👋Hello，我是Peri，一位自學的前端工程師。打造美感與實用兼具的網頁程式是我的熱情所在！歡迎一起開創網路世界！`;
 
-export const resumeLink = ' ';
-
-export const about = `I'm passionate about frontend development and have created projects on React, Next.js, and web app development.`;
+export const resumeLink =
+  'https://www.cakeresume.com/s--bYh-WPftKIvV0N2yzYbTSQ--/peri-333a33';
 
 export const playlist = 'https://my-youtube-collection.vercel.app/';
 
 /* contact */
-export const contactIntro = `I'm currently open for a fulltime Frontend Developer role. If you want to discuss about that, feel free to contact me.`;
+export const contactIntro = `我目前正積極尋找前端工程師職位🏃‍♀️如果您有任何關於網頁開發的工作機會、合作提案，或者對我的作品有任何問題，歡迎與我聯繫：）`;
 
 export const email = 'periondev@gmail.com';
 
